e53879a6a2dd73d2448cb0181774390a4cb270e2
Pedro Capriles <caprilespe@outlook.com>
2019-09-18 15:32:52 -0300

    Creating an array of objects

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace CorEscuela
         {
             var escuela = new Escuela("Escuela Platzi",2006,TipoEscuelas.PreEscolar,pais:"Colombia",ciudad:"Bogotá");
 
+            var arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso(){
+                Nombre = "101"
+            };
+
             var curso1 = new Curso(){
                 Nombre = "101",
             };
@@ -17,14 +23,29 @@ namespace CorEscuela
             var curso2 = new Curso(){
                 Nombre = "201",
             };
+            arregloCursos[1] = curso2;
 
             var curso3 = new Curso(){
                 Nombre = "301",
             };
+
+            arregloCursos[2] = new Curso(){
+                Nombre = "301"
+            };
             Console.WriteLine(escuela);
             System.Console.WriteLine("================");
-            System.Console.WriteLine($"{curso1.Nombre}, {curso1.UniqueId}");
-            System.Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+
+            ImprimirCursos(arregloCursos);
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
+                contador++;
+            }
         }
     }
 }
